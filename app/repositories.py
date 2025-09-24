264bf96ec6674b4ce1a777d290f48e333bc1691b
itsaboutarman <armansalehi82@aut.ac.ir>
2025-09-24 09:07:49 +0330

    feat: implement sqlalchemy task repository

--- app/repositories.py
+++ app/repositories.py
@@ -92,3 +92,35 @@ class SqlAlchemyProjectRepository(ProjectRepository):
         if project:
             self.session.delete(project)
             self.session.flush()
+
+class SqlAlchemyTaskRepository(TaskRepository):
+    # The concrete SQLAlchemy implementation of the Task Repository.
+    def __init__(self, session: Session):
+        self.session = session
+
+    def add(self, task: Task) -> Task:
+        self.session.add(task)
+        self.session.flush()
+        return task
+
+    def get_by_id(self, task_id: int) -> Optional[Task]:
+        return self.session.get(Task, task_id)
+
+    def get_by_name(self, task_name: str) -> Optional[Task]:
+        statement = select(Task).where(Task.title == task_name)
+        return self.session.scalar(statement)
+
+    def get_all_for_project(self, project_id: int) -> List[Task]:
+        statement = select(Task).where(Task.project_id == project_id)
+        return list(self.session.scalars(statement).all())
+
+    def update(self, task: Task) -> Task:
+        self.session.add(task)
+        self.session.flush()
+        return task
+
+    def delete(self, task_id: int) -> None:
+        task = self.get_by_id(task_id)
+        if task:
+            self.session.delete(task)
+            self.session.flush()
