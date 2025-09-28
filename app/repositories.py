526b3839f8266d4e2c6ef60b6091a500fea4052e
itsaboutarman <armansalehi82@aut.ac.ir>
2025-09-28 16:15:30 +0330

    refactor: separate repository interfaces from implementations

--- app/repositories.py
+++ app/repositories.py
@@ -1,6 +1,4 @@
 from typing import Optional, List
-from sqlalchemy import select
-from sqlalchemy.orm import Session
 from .models import Project, Task
 from abc import ABC, abstractmethod
 
@@ -58,69 +56,3 @@ class TaskRepository(ABC):
     @abstractmethod
     def delete(self, task_id: int) -> None:
         raise NotImplementedError
-
-
-
-
-class SqlAlchemyProjectRepository(ProjectRepository):
-    def __init__(self, session: Session):
-        self.session = session
-
-    def add(self, project: Project) -> Project:
-        self.session.add(project)
-        self.session.flush()
-        return project
-
-    def get_by_id(self, project_id: int) -> Optional[Project]:
-        return self.session.get(Project, project_id)
-
-    def get_by_name(self, project_name: str) -> Optional[Project]:
-        statement = select(Project).filter_by(name=project_name)
-        return self.session.scalars(statement).first()
-
-    def get_all(self) -> List[Project]:
-        statement = select(Project)
-        return list(self.session.scalars(statement).all())
-
-    def update(self, project: Project) -> Project:
-        self.session.add(project)
-        self.session.flush()
-        return project
-
-    def delete(self, project_id: int) -> None:
-        project = self.get_by_id(project_id)
-        if project:
-            self.session.delete(project)
-            self.session.flush()
-
-class SqlAlchemyTaskRepository(TaskRepository):
-    # The concrete SQLAlchemy implementation of the Task Repository.
-    def __init__(self, session: Session):
-        self.session = session
-
-    def add(self, task: Task) -> Task:
-        self.session.add(task)
-        self.session.flush()
-        return task
-
-    def get_by_id(self, task_id: int) -> Optional[Task]:
-        return self.session.get(Task, task_id)
-
-    def get_by_name(self, task_name: str) -> Optional[Task]:
-        statement = select(Task).where(Task.title == task_name)
-        return self.session.scalar(statement)
-
-    def get_all_for_project(self, project_id: int) -> List[Task]:
-        statement = select(Task).where(Task.project_id == project_id)
-        return list(self.session.scalars(statement).all())
-
-    def update(self, task: Task) -> Task:
-        self.session.add(task)
-        self.session.flush()
-        return task
-
-    def delete(self, task_id: int) -> None:
-        task = self.get_by_id(task_id)
-        if task:
-            self.session.delete(task)
-            self.session.flush()
